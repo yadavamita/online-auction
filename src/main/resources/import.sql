0549edd7bc65b33bcb8b7fc7b83400d3ccc68813
ve-amita <amitayadav@virtualemployee.com>
2020-02-27 14:10:26 +0530

    Add Password Encoding in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO `registration`(`email_address`, `public_name`,`password`) VALUES("admin@gmail.com","admin","Abc111");
+INSERT INTO `registration`(`email_address`, `public_name`,`password`) VALUES("admin@gmail.com","admin","$2a$10$DorWCtNPKZkeimULFIe69.FvlhMs8rHqD2siMhem8eeGfEkjgWXC6");
